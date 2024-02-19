83401a752aabfd74f97945beb3cb662a45cb9d8d
EDMerstratec <erme0001@edu.mercantec.dk>
2024-02-19 15:01:43 +0100

    day1 end

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -34,14 +34,14 @@ Console.Write(" The temperature is " + temperatureCels + " celsius.");
 */
 
 
-/* string projectName = "ACME";
+string projectName = "ACME";
 
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
 Console.Write("View English output:\n\t" + $@"c:\Exercise\{projectName}\data.txt");
 
 Console.Write($"\n\n{russianMessage}:\n\t" + $@"c:\Exercise\{projectName}\ru-RU\data.txt");
-*/
+
 
 /* int fahrenheit = 94;
 float toCelsius = ((fahrenheit-32.0f) * 5/9);
