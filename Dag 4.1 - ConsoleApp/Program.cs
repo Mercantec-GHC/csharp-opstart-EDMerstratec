4f3bbd575e164ce22fc7e459406522077d838ba4
EDMerstratec <erme0001@edu.mercantec.dk>
2024-02-22 11:02:15 +0100

    Day4,4/7

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -127,3 +127,153 @@ float result3 = 0;
 result3 = (value3 / (float)value1);
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 */
+
+
+
+//4.3 Arrays
+
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
+//.sort, .reverse, .clear, .resize
+
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
+
+/*if (pallets[0] != null)
+    Console.WriteLine($"After: {pallets[0].ToLower()}");*/
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+//removing null vals in array includes foreach check for null and assign new array
+*/
+/*
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+*/
+
+/*
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] splitWords = pangram.Split(" ");
+string outputPangram = "";
+foreach (string word in splitWords)
+{
+    char[] chars = word.ToCharArray();
+    Array.Reverse(chars);
+    //Console.WriteLine(chars);
+    outputPangram += (new string(chars) + " ");
+}
+Console.Write(outputPangram);
+//Console.WriteLine(splitWords[3]);
+*/
+
+/*
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] checkOrders = orderStream.Split(",");
+foreach (string checkOrder in checkOrders)
+{
+    Console.Write(checkOrder);
+    if (checkOrder.Length != 4) { Console.Write("\t- Error"); }
+    Console.Write("\n");
+}
+*/
+
+// 4.4 Alphanumeric formatting
+
+/*
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+*/
+//string interpolation $"{first}" preferred
+
+/*
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+*/
+
+/* Round percentage
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P1}");
+*/
+
+//SELF round numbers with {myNumber:N3}
+
+/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+*/
+
+/*
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+*/
